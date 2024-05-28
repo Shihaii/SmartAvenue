4e19a4b0829f0a9b415def42877ebf2a29b772c6
Shihai <Kirill2004val@gmail.com>
2024-05-28 16:02:36 +0300

    fix: js to jquery

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
-// import Swiper from 'swiper/bundle';
-// import Swiper from '/node_modules/swiper';
+function initHandleScroll() {
+  const handleScroll = () => {
+    let value = window.scrollY;
+    if (value > 0) {
+      document.getElementById("header").classList.add("header__white");
+    } else {
+      document.getElementById("header").classList.remove("header__white");
+    }
+  };
+  document.addEventListener("scroll", handleScroll);
+}
 
-console.log("hi");
-const handleScroll = () => {
-  console.log("hi");
-  let value = window.scrollY;
-  console.log("value", value);
-  if (value > 0) {
-    document.getElementById("header").classList.add("header__white");
-  } else {
-    document.getElementById("header").classList.remove("header__white");
-  }
-};
-document.addEventListener("scroll", handleScroll);
-
-
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  on: {
-    init: function () {
-      console.log('swiper initialized');
+function initSwiper() {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    on: {
+      init: function () {
+        console.log("swiper initialized");
+      },
     },
-  },
-  direction: 'horizontal',
-  loop: true,
-  allowTouchMove: false,
-  autoplay: {
-    delay: 1000000,
-  },
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
+    direction: "horizontal",
+    loop: true,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 10000,
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
 
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
+$(document).ready(function () {
+  initHandleScroll();
+  initSwiper();
 });
