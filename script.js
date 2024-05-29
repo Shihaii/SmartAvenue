ad40f308382248481ecd4214a7ef4d082d6b9a78
Shihai <Kirill2004val@gmail.com>
2024-05-29 11:33:41 +0300

    fix(header): text color changing with header background color, changed font weight to phone number

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
+function initHeaderColors(){
+  $(".header-item-menu__text").addClass("text-color-secondary-dark");
+  $(".header-item-menu__text:hover").addClass("text-color-primary-dark");
+  $(".header-item__text-general").addClass("text-color-primary-dark");
+  $(".header-item__select").addClass("text-color-primary-dark");
+  $(".header-item__label").addClass("text-color-secondary-dark")
+}
+
 function initHandleScroll() {
   const handleScroll = () => {
     let value = window.scrollY;
-    if (value > 0) {
-      document.getElementById("header").classList.add("header__white");
+    if (value > 5) {
+      $("#header").addClass("header__white");
+
+      $(".header-item-menu__text").removeClass("text-color-secondary-dark");
+      $(".header-item-menu__text:hover").removeClass("text-color-primary-dark");
+      $(".header-item__text-general").removeClass("text-color-primary-dark");
+      $(".header-item__select").removeClass("text-color-primary-dark");
+      $(".header-item__label").removeClass("text-color-secondary-dark")
+
+      $(".header-item-menu__text").addClass("text-color-secondary-light");
+      $(".header-item-menu__text:hover").addClass("text-color-primary-light");
+      $(".header-item__text-general").addClass("text-color-primary-light");
+      $(".header-item__select").addClass("text-color-primary-light");
+      $(".header-item__label").addClass("text-color-secondary-light")
     } else {
-      document.getElementById("header").classList.remove("header__white");
+      $("#header").removeClass("header__white");
+   
+      $(".header-item-menu__text").removeClass("text-color-secondary-light");
+      $(".header-item-menu__text:hover").removeClass("text-color-primary-light");
+      $(".header-item__text-general").removeClass("text-color-primary-light");
+      $(".header-item__select").removeClass("text-color-primary-light");
+      $(".header-item__label").removeClass("text-color-secondary-light")
+
+      $(".header-item-menu__text").addClass("text-color-secondary-dark");
+      $(".header-item-menu__text:hover").addClass("text-color-primary-dark");
+      $(".header-item__text-general").addClass("text-color-primary-dark");
+      $(".header-item__select").addClass("text-color-primary-dark");
+      $(".header-item__label").addClass("text-color-secondary-dark")
     }
   };
-  document.addEventListener("scroll", handleScroll);
+  $(document).scroll(handleScroll);
 }
 
 function initSwiper() {
@@ -34,6 +66,7 @@ function initSwiper() {
 }
 
 $(document).ready(function () {
+  initHeaderColors();
   initHandleScroll();
   initSwiper();
 });
