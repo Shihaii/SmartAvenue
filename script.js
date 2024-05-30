dd54f109d2eab5c7cc11aa19d878c772a508f2e5
Shihai <Kirill2004val@gmail.com>
2024-05-30 14:38:49 +0300

    feat: responsive header & first-section

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-function initHeaderColors(){
+function initHeaderColors() {
   $(".header-item-menu__text").addClass("text-color-secondary-dark");
   $(".header-item-menu__text:hover").addClass("text-color-primary-dark");
   $(".header-item__text-general").addClass("text-color-primary-dark");
   $(".header-item__select").addClass("text-color-primary-dark");
-  $(".header-item__label").addClass("text-color-secondary-dark")
+  $(".header-item__label").addClass("text-color-secondary-dark");
 }
 
 function initHandleScroll() {
   const handleScroll = () => {
     let value = window.scrollY;
+    let vw = Math.max(
+      document.documentElement.clientWidth || 0,
+      window.innerWidth || 0
+    );
     if (value > 5) {
       $("#header").addClass("header__white");
 
-      $(".header-item-menu__text").removeClass("text-color-secondary-dark");
-      $(".header-item-menu__text:hover").removeClass("text-color-primary-dark");
-      $(".header-item__text-general").removeClass("text-color-primary-dark");
-      $(".header-item__select").removeClass("text-color-primary-dark");
-      $(".header-item__label").removeClass("text-color-secondary-dark")
-
-      $(".header-item-menu__text").addClass("text-color-secondary-light");
-      $(".header-item-menu__text:hover").addClass("text-color-primary-light");
-      $(".header-item__text-general").addClass("text-color-primary-light");
-      $(".header-item__select").addClass("text-color-primary-light");
-      $(".header-item__label").addClass("text-color-secondary-light")
+      if (vw > 900) {
+        $(".header-item-menu__text")
+          .removeClass("text-color-secondary-dark")
+          .addClass("text-color-secondary-light");
+        $(".header-item-menu__text:hover")
+          .removeClass("text-color-primary-dark")
+          .addClass("text-color-primary-light");
+        $(".header-item__text-general")
+          .removeClass("text-color-primary-dark")
+          .addClass("text-color-primary-light");
+        $(".header-item__select")
+          .removeClass("text-color-primary-dark")
+          .addClass("text-color-primary-light");
+        $(".header-item__label")
+          .removeClass("text-color-secondary-dark")
+          .addClass("text-color-secondary-light");
+      }
     } else {
       $("#header").removeClass("header__white");
-   
-      $(".header-item-menu__text").removeClass("text-color-secondary-light");
-      $(".header-item-menu__text:hover").removeClass("text-color-primary-light");
-      $(".header-item__text-general").removeClass("text-color-primary-light");
-      $(".header-item__select").removeClass("text-color-primary-light");
-      $(".header-item__label").removeClass("text-color-secondary-light")
 
-      $(".header-item-menu__text").addClass("text-color-secondary-dark");
-      $(".header-item-menu__text:hover").addClass("text-color-primary-dark");
-      $(".header-item__text-general").addClass("text-color-primary-dark");
-      $(".header-item__select").addClass("text-color-primary-dark");
-      $(".header-item__label").addClass("text-color-secondary-dark")
+      if (vw > 900) {
+        $(".header-item-menu__text")
+          .removeClass("text-color-secondary-light")
+          .addClass("text-color-secondary-dark");
+        $(".header-item-menu__text:hover")
+          .removeClass("text-color-primary-light")
+          .addClass("text-color-primary-dark");
+        $(".header-item__text-general")
+          .removeClass("text-color-primary-light")
+          .addClass("text-color-primary-dark");
+        $(".header-item__select")
+          .removeClass("text-color-primary-light")
+          .addClass("text-color-primary-dark");
+        $(".header-item__label")
+          .removeClass("text-color-secondary-light")
+          .addClass("text-color-secondary-dark");
+      }
     }
   };
   $(document).scroll(handleScroll);
@@ -64,9 +80,85 @@ function initSwiper() {
     },
   });
 }
+function openMenu() {
+  $("#header").toggleClass("header").toggleClass("header-responsive");
+  $("#header-item-menu")
+    .toggleClass("header-item-menu")
+    .toggleClass("header-item-menu-responsive");
+  $("#header-item-group")
+    .toggleClass("header-item-group")
+    .toggleClass("header-item-group-responsive");
+  $("#header-divider")
+    .toggleClass("header-divider-container")
+    .toggleClass("header-divider-container-responsive");
+  $("#header-item__social-media")
+    .toggleClass("header-item__social-media")
+    .toggleClass("header-item__social-media-responsive");
+  $("#header-item__language")
+    .toggleClass("header-item__language")
+    .toggleClass("header-item__language-responsive");
+  $("#header-item__address")
+    .toggleClass("header-item__address")
+    .toggleClass("header-item__address-responsive");
+  $("#header-item__phone")
+    .toggleClass("header-item__phone")
+    .toggleClass("header-item__phone-responsive");
+}
+function initMenuButton() {
+  $("#menu-button").click(function () {
+    $("body").toggleClass("disable-scroll");
+    $("#menu-button").toggleClass("show").toggleClass("hide");
+    $("#cross-button").toggleClass("show").toggleClass("hide");
+    openMenu();
+    $(".header-item-menu__text")
+      .removeClass("text-color-secondary-dark")
+      .addClass("text-color-secondary-light");
+    $(".header-item-menu__text")
+      .removeClass("text-color-secondary-dark")
+      .addClass("text-color-secondary-light");
+    $(".header-item-menu__text:hover")
+      .removeClass("text-color-primary-dark")
+      .addClass("text-color-primary-light");
+    $(".header-item__text-general")
+      .removeClass("text-color-primary-dark")
+      .addClass("text-color-primary-light");
+    $(".header-item__select")
+      .removeClass("text-color-primary-dark")
+      .addClass("text-color-primary-light");
+    $(".header-item__label")
+      .removeClass("text-color-secondary-dark")
+      .addClass("text-color-secondary-light");
+  });
+
+  $("#cross-button").click(function () {
+    $("body").toggleClass("disable-scroll");
+    $("#menu-button").toggleClass("show").toggleClass("hide");
+    $("#cross-button").toggleClass("show").toggleClass("hide");
+    openMenu();
+    $(".header-item-menu__text")
+      .removeClass("text-color-secondary-light")
+      .addClass("text-color-secondary-dark");
+    $(".header-item-menu__text")
+      .removeClass("text-color-secondary-light")
+      .addClass("text-color-secondary-dark");
+    $(".header-item-menu__text:hover")
+      .removeClass("text-color-primary-light")
+      .addClass("text-color-primary-dark");
+    $(".header-item__text-general")
+      .removeClass("text-color-primary-light")
+      .addClass("text-color-primary-dark");
+    $(".header-item__select")
+      .removeClass("text-color-primary-light")
+      .addClass("text-color-primary-dark");
+    $(".header-item__label")
+      .removeClass("text-color-secondary-light")
+      .addClass("text-color-secondary-dark");
+  });
+}
 
 $(document).ready(function () {
   initHeaderColors();
   initHandleScroll();
   initSwiper();
+  initMenuButton();
 });
